b4d4a61c9f73108e2357d7efd4d9f811df173ca0
David Lara <oscar.lr97@gmail.com>
2019-03-18 14:00:18 -0600

    Agregado NFC y HTTP Request en NFC.ts

--- navegacion3/src/pages/nfc/nfc.ts
+++ navegacion3/src/pages/nfc/nfc.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
-
+import { NFC, Ndef } from "@ionic-native/nfc/ngx";
 import { PerfilesPage } from "../index.paginas";
 
 
@@ -12,7 +12,9 @@ import { PerfilesPage } from "../index.paginas";
 export class nfcPage {
 
   perfiles:any = PerfilesPage;
-
+//aqui se estara escuchando el nfc 
+//una vez que lo encuentre, se hace el ajax request con el que recibió 
+//y se abre la pantalla de Perfil con el json llenando los campos 
   mutantes:any[] = [
     {
       nombre:"Ernesto de la Cruz",
@@ -35,10 +37,27 @@ export class nfcPage {
       epitafio:"Una tumba es suficiente para quien el Universo no bastara."
     }]; 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private nfc: NFC, private ndef: Ndef) {
+      //aqui escucha al nfc 
+      this.nfc.addNdefListener(() => {
+        console.log('attached ndef listener');
+      }, (err) => {
+        console.log('error attaching ndef listener', err);
+      }).subscribe((event) => {
+        console.log('received message. Tag contains: ', event.tag);
+        console.log('tag id: ', this.nfc.bytesToHexString(event.tag.id));
+        this.ajaxCall(this.nfc.bytesToHexString(event.tag.id));
+      });
+      
   }
 
-  irPagina3( mutante:any ){
+  ajaxCall(tag:any){
+
+  }
+  
+
+  irPaginaPerfil( mutante:any ){
     console.log( mutante );
 
       this.navCtrl.push( PerfilesPage, { 'mutante': mutante } );
